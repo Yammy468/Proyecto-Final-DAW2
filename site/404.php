718c0ba08b7b847b56ff26bb37b9c641b333b70b
Yammy468 <yzhu0806@g.educaand.es>
2024-05-17 14:01:39 +0200

    subir archivo index y 404

---
+++ site/404.php
@@ -0,0 +1,273 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <!-- Always force latest IE rendering engine or request Chrome Frame -->
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+  <!-- Use title if it's in the page YAML frontmatter -->
+  <title>404 Not Found</title>
+
+  <!-- Favicon -->
+  <link href="img/favicon.png" rel="icon">
+
+  <!-- Fuentes de Google -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Archivos CSS Vendor -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Archivos CSS Pricipales -->
+  <link rel="stylesheet" href="css/estilos_principal.css">
+  <link rel="stylesheet" href="css/b00tstraps.css">
+  <link rel="stylesheet" href="css/fonts.css">
+  <style>
+    #margen-arriba {
+      margin-top: 110px;
+      margin-bottom: 80px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="page">
+    <!-- ======= Header ======= -->
+    <header class="section page-header">
+      <!-- Navbar-->
+      <div class="rd-navbar-wrap">
+        <nav class="rd-navbar rd-navbar-corporate" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed"
+          data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static"
+          data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static"
+          data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static"
+          data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px"
+          data-xxl-stick-up-offset="106px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+          <!-- Navbar Toggle en la Derecha -->
+          <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse">
+            <span></span>
+          </div>
+          <div class="rd-navbar-aside-outer">
+            <div class="rd-navbar-aside">
+              <div class="rd-navbar-panel">
+                <!-- Icono de Menu -->
+                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                <!-- Imagen/Marca Navbar -->
+                <div class="rd-navbar-brand">
+                  <!-- Logo -->
+                  <a href="#"><img src="img/logo.png" alt="" width="300" height="80" /></a>
+                </div>
+              </div>
+              <!-- Contenido del Navbar Derecho -->
+              <div class="rd-navbar-aside-right rd-navbar-collapse">
+                <ul class="rd-navbar-corporate-contacts">
+                  <li>
+                    <div class="unit unit-spacing-xs">
+                      <div class="unit-left"><span class="icon fa fa-clock-o"></span></div>
+                      <div class="unit-body">
+                        <p>Lunes - Viernes:</p>
+                        <p><span>09:00 — 20:00</span></p>
+                        <p>Sábado:</p>
+                        <p><span>09:30 — 14:30</span></p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div class="unit unit-spacing-xs">
+                      <div class="unit-left"><span class="icon fa fa-phone"></span></div>
+                      <div class="unit-body">
+                        <a class="link-phone" href="#">+34 907 98 00 32</a>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+
+                <a class="button button-md button-default-outline-2 button-ujarak" href="#">Iniciar Sesión</a>
+              </div>
+            </div>
+          </div>
+          <!-- Contenido del Menu -->
+          <div class="rd-navbar-main-outer">
+            <div class="rd-navbar-main">
+              <div class="rd-navbar-nav-wrap">
+                <!-- Iconos de Redes Sociales -->
+                <ul class="list-inline list-inline-md rd-navbar-corporate-list-social">
+                  <li><a class="icon bi bi-facebook" href="#"></a></li>
+                  <li><a class="icon bi bi-twitter" href="#"></a></li>
+                  <li><a class="icon bi bi-github" href="#"></a></li>
+                  <li><a class="icon bi bi-instagram" href="#"></a></li>
+                </ul>
+                <!-- Navbar Nav-->
+                <ul class="rd-navbar-nav">
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Inicio</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Sobre nosotros</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Departamentos</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Servicios</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Blog</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Contacto</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#"><i class="bi bi-cart4"></i> Carrito</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div> <!-- End Navbar-->
+    </header>
+  </div>
+
+  <!-- Main -->
+  <main id="main">
+    <section class="error">
+      <div class="container" id="margen-arriba">
+        <div class="row">
+          <div class="col-12 d-flex flex-column align-items-center">
+            <h1 style="color: red; font-family: Helvetica, Arial; font-size: 80px; font-weight: bold;">Error 404
+            </h1>
+            <h3 style="color: gray; font-size: 20px; margin-bottom: 40px;">La ubicación que buscas no existe :(</h3>
+
+            <div class="group-md group-middle justify-content-sm-start">
+              <a href="index.php" class="button button-lg button-primary button-ujarak" style="border-radius: 20px;">Regresar a la página de inicio</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <div class="page">
+    <!-- ======= Footer ======= -->
+    <footer class="section footer-corporate context-dark">
+      <div class="footer-corporate-inset">
+        <div id="footer" class="container">
+          <!-- Más Info -->
+          <div class="row row-40 justify-content-lg-between">
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-4">
+              <div class="oh-desktop">
+                <div class="wow slideInRight" data-wow-delay="0s">
+                  <h6 class="text-spacing-100 text-uppercase">Contacta con nosotros</h6>
+                  <ul class="footer-contacts d-inline-block">
+                    <li>
+                      <div class="unit">
+                        <div class="unit-left"><span class="icon bi bi-telephone-fill"></span></div>
+                        <div class="unit-body"><a class="link-phone" href="#">+34 907 98 00 32</a></div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="unit">
+                        <div class="unit-left"><span class="icon bi bi-whatsapp"></span></div>
+                        <div class="unit-body"><a class="link-phone" href="#">+34 667 73 44 11</a></div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="unit">
+                        <div class="unit-left"><span class="icon bi bi-envelope-fill"></span></div>
+                        <div class="unit-body"><a class="link-aemail" href="#">info@DentShiney.com</a></div>
+                      </div>
+                      <div class="unit">
+                        <div class="unit-left"><span class="icon bi bi"></span></div>
+                        <div class="unit-body"><a class="link-aemail" href="#">pedircita@DentShiney.com</a></div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="unit">
+                        <div class="unit-left"><span class="icon bi bi-geo-alt-fill"></span></div>
+                        <div class="unit-body"><a class="link-location" href="#">C. de Bravo Murillo, 151, Tetuán, 28020 Madrid</a></div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-4">
+              <div class="oh-desktop">
+                <div class="wow slideInDown" data-wow-delay="0s">
+                  <h6 class="text-spacing-100 text-uppercase">Noticias Populares</h6>
+                  <!-- Post Minimal 2-->
+                  <article class="post post-minimal-2">
+                    <p class="post-minimal-2-title"><a href="#">Datos Curiosos Sobre Los Dientes Y La Odontología</a></p>
+                    <div class="post-minimal-2-time">
+                      <time datetime="2019-05-04">29 Marzo, 2023</time>
+                    </div>
+                  </article>
+                  <!-- Post Minimal 2-->
+                  <article class="post post-minimal-2">
+                    <p class="post-minimal-2-title"><a href="#">La revolución de la odontología</a></p>
+                    <div class="post-minimal-2-time">
+                      <time datetime="2019-05-04">10 Marzo, 2024</time>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-4">
+              <div class="oh-desktop">
+                <div class="wow slideInLeft" data-wow-delay="0s">
+                  <div class="group-md group-middle justify-content-sm-center">
+                    <img src="img/footer-img.png" alt="" width="300px">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> <!-- End Más Info -->
+        </div>
+      </div>
+
+      <!-- Footer 02 -->
+      <div class="footer-corporate-bottom-panel">
+        <div class="container margen-arriba">
+          <div class="row justfy-content-xl-space-berween row-10 align-items-sm-center2">
+            <div class="col-sm-12 col-md-4 text-sm-center text-md-center">
+              <div>
+                <ul class="list-inline list-inline-sm footer-social-list-2">
+                  <li><a class="icon bi bi-facebook" href="#"></a></li>
+                  <li><a class="icon bi bi-twitter" href="#"></a></li>
+                  <li><a class="icon bi bi-github" href="#"></a></li>
+                  <li><a class="icon bi bi-instagram" href="#"></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4 order-md-first">
+              <!-- Autor de la página -->
+              <p class="rights">
+                <span>&copy;&nbsp;</span>
+                <span class="copyright-year"></span>
+                <span>&nbsp;</span><span>DentShiney</span><br>
+                Diseñado por <a href="#">Yana Zhu</a>.
+              </p>
+            </div>
+            <div class="col-sm-12 col-md-4 text-sm-center">
+              <p class="rights"><a href="#">Política de Privacidad</a></p>
+            </div>
+          </div>
+        </div>
+      </div> <!-- End Footer 02 -->
+    </footer> <!-- End Footer -->
+  </div>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  
+  <!-- Archivos Vendor JS -->
+  <script src="vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+
+  <!-- Archivos Template Main JS -->
+  <script src="js/main.js"></script>
+  <script src="js/core.min.js"></script>
+  <script src="js/script.js"></script>
+
+</body>
+
+</html>
